b346772817c7d1ff524569d1632393fc38757327
Ricardo Ferreira <ricardo.sff@gmail.com>
2015-04-05 18:20:56 +0100

    library linking should be in the end. some attempt to try to detect TFO

--- test_TFO.c
+++ test_TFO.c
@@ -2,12 +2,19 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+#ifndef TCP_FASTOPEN
+#define TCP_FASTOPEN	23
+#endif
 
 int main(int argc, char *argv[])
 {
+	int sfd = 0;
 	int qlen = 5;
 
-	setsockopt(sfd, SOL_TCP, TCP_FASTOPEN, &qlen, sizeof(qlen));
+	setsockopt(sfd, IPPROTO_TCP, TCP_FASTOPEN, &qlen, sizeof(qlen));
 
 	return 0;
 }
